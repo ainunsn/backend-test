35d06a75f47d0d32e32f62ed7d4c3f19445d71dc
Ainun <ainun.s.n@gmail.com>
2021-02-10 18:38:25 +0700

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const db = require("./config/database");
 const Router = require("./routes/routes");
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 // Testing database connection
 
